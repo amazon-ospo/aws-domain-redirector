4946ad60f044e5addc3dc79858d15e56e777cdbe
mattsb42-aws <bullocm@amazon.com>
2021-08-30 15:54:16 -0700

    chore: update projenrc with CDK dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -2,13 +2,20 @@ const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'mattsb42-aws',
   authorAddress: 'bullocm@amazon.com',
-  cdkVersion: '1.95.2',
   defaultReleaseBranch: 'main',
   name: 'aws-domain-redirector',
-  repositoryUrl: 'git@github.com:mattsb42-aws/aws-domain-redirector.git',
-
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
-  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
+  repository: 'https://github.com/awslabs/aws-domain-redirector',
+  cdkVersion: '1.95.2',
+  cdkAssert: true,
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-certificatemanager',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-elasticloadbalancingv2',
+    '@aws-cdk/aws-route53',
+    '@aws-cdk/aws-route53-targets',
+  ],
+  gitignore: ['.idea'],
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
